44f14d4b8a4c8a78361e115afbb0339ae1cad241
Petr Zaznobin <69428320+Petr-Zaznobin@users.noreply.github.com>
2023-10-16 00:09:47 +0300

    Update sample_prime_numbers.cpp

--- samples/sample_prime_numbers.cpp
+++ samples/sample_prime_numbers.cpp
@@ -1,4 +1,4 @@
-﻿// ННГУ, ВМК, Курс "Методы программирования-2", С++, ООП
+// ННГУ, ВМК, Курс "Методы программирования-2", С++, ООП
 //
 // sample_prime_numbers.cpp - Copyright (c) Гергель В.П. 20.08.2000
 //   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (19.04.2015)
@@ -8,7 +8,7 @@
 #include <iomanip>
 
 // #define USE_SET // Использовать класс TSet,
-                // закоментировать, чтобы использовать битовое поле
+// закоментировать, чтобы использовать битовое поле
 
 #ifndef USE_SET // Использовать класс TBitField
 
@@ -16,39 +16,49 @@
 
 int main()
 {
-  int n, m, k, count;
-
-  setlocale(LC_ALL, "Russian");
-  cout << "Тестирование программ поддержки битового поля" << endl;
-  cout << "             Решето Эратосфена" << endl;
-  cout << "Введите верхнюю границу целых значений - ";
-  cin  >> n;
-  TBitField s(n + 1);
-  // заполнение множества
-  for (m = 2; m <= n; m++)
-    s.SetBit(m);
-  // проверка до sqrt(n) и удаление кратных
-  for (m = 2; m * m <= n; m++)
-    // если m в s, удаление кратных
-    if (s.GetBit(m))
-      for (k = 2 * m; k <= n; k += m)
-        if (s.GetBit(k))
-          s.ClrBit(k);
-  // оставшиеся в s элементы - простые числа
-  cout << endl << "Печать множества некратных чисел" << endl << s << endl;
-  cout << endl << "Печать простых чисел" << endl;
-  count = 0;
-  k = 1;
-  for (m = 2; m <= n; m++)
-    if (s.GetBit(m))
-    {
-      count++;
-      cout << setw(3) << m << " ";
-      if (k++ % 10 == 0)
-        cout << endl;
+    int n, m, k, count;
+    setlocale(LC_ALL, "Russian");
+    cout << "Тестирование программ поддержки битового поля" << endl;
+    cout << "             Решето Эратосфена" << endl;
+    cout << "Введите верхнюю границу целых значений - ";
+    cin  >> n;
+    TBitField s(n + 1);
+    TBitField r(n+1);
+    // заполнение множества
+    try{
+        for (m = 2; m <= n; m++)
+            s.SetBit(m);
+        // проверка до sqrt(n) и удаление кратных
+        for (m = 2; m * m <= n; m++)
+            // если m в s, удаление кратных
+            if (s.GetBit(m))
+                for (k = 2 * m; k <= n; k += m)
+                    if (s.GetBit(k))
+                        s.ClrBit(k);
     }
-  cout << endl;
-  cout << "В первых " << n << " числах " << count << " простых" << endl;
+    catch(const char* error_message) {
+        cout << error_message<<endl;
+    }
+    // оставшиеся в s элементы - простые числа
+    cout << endl << "Печать множества некратных чисел" << endl << s << endl;
+    cout << endl << "Печать простых чисел" << endl;
+    count = 0;
+    k = 1;
+    try{
+        for (m = 2; m <= n; m++)
+            if (s.GetBit(m))
+            {
+                count++;
+                cout << setw(3) << m << " ";
+                if (k++ % 10 == 0)
+                    cout << endl;
+            }
+    }
+    catch(const char* error_message) {
+        cout << error_message<<endl;
+    }
+    cout << endl;
+    cout << "В первых " << n << " числах " << count << " простых" << endl;
 }
 #else
 
