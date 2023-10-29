4cd755ca3a66471c827997e9826df6e27424944f
SosnovskayaUlyana <119797588+SosnovskayaUlyana@users.noreply.github.com>
2023-10-29 20:23:37 +0300

    Update tbitfield.cpp
    error correction

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -55,24 +55,19 @@ int TBitField::GetLength(void) const // получить длину (к-во б�
 
 void TBitField::SetBit(const int n) // установить бит
 {
-	   if ((n < 0) || (n >= BitLen))
-    {
-        throw out_of_range("out of range");
-    }
+
 	pMem[GetMemIndex(n)] |= GetMemMask(n);
 }
 
 void TBitField::ClrBit(const int n) // очистить бит
 {
-    if ((n < 0) || (n >= BitLen))
-        throw out_of_range("out of range");
+    
     pMem[GetMemIndex(n)] &= ~GetMemMask(n);
 }
 
 int TBitField::GetBit(const int n) const // получить значение бита
 {
-    if ((n < 0) || (n >= BitLen))
-        throw out_of_range("out of range");
+  
     return (pMem[GetMemIndex(n)] & GetMemMask(n));
 }
 
